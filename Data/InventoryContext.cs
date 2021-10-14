1d80cf6ceb49f664fbaeeb18a21f9c41ee6b1aeb
Jonas Van Hove <r0741524@student.thomasmore.be>
2021-10-14 11:47:20 +0200

    Start Cosmident

---
+++ Data/InventoryContext.cs
@@ -0,0 +1,19 @@
+﻿using COSMIDENT.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COSMIDENT.Data
+{
+    public class InventoryContext:DbContext
+    {
+        public InventoryContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public virtual DbSet<Unit> Units { get; set; }
+    }
+}
